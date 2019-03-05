c4281c196ee7446133683fcea2b9390440c24b45
Stephanie Meagher <stephanie.meagher@gmail.com>
2019-03-05 11:55:26 +0100

    fixed listing model relationship syntax error

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
-  has_many :photos, :bookings
+  has_many :photos
+  has_many :bookings
   validates :name, :description, :capacity, :addresss, :nightly_rate, presence: true
   validates :type, presence: true, inclusion: { in: ["Apartment", "House", "Secondary unit", "Unique space", "Bed and breakfast", "Boutique hotel"] }
   validates :capacity, :nightly_rate, numericality: { only_integer: true }
