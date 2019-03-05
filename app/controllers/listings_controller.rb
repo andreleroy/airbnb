9039dcb858486a9abc8a10acf51ea2626033d112
Stephanie Meagher <stephanie.meagher@gmail.com>
2019-03-05 11:20:02 +0100

    create the  new action for listings with permitted params

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -6,8 +6,18 @@ class ListingsController < ApplicationController
   end
 
   def new
+    @listing = Listing.new(listing_params)
   end
 
   def create
   end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:name, :description, :type, :capacity, :address, :nightly_rate, :latitude, :longitude)
+  end
+
 end
+
+
