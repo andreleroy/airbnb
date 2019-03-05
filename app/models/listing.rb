2a790de16b10d58992202ebe3ec6fe05eb841263
Stephanie Meagher <stephanie.meagher@gmail.com>
2019-03-05 11:56:39 +0100

    fix listing moddel validation syntax error on address

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_many :photos
   has_many :bookings
-  validates :name, :description, :capacity, :addresss, :nightly_rate, presence: true
+  validates :name, :description, :capacity, :address, :nightly_rate, presence: true
   validates :type, presence: true, inclusion: { in: ["Apartment", "House", "Secondary unit", "Unique space", "Bed and breakfast", "Boutique hotel"] }
   validates :capacity, :nightly_rate, numericality: { only_integer: true }
 end
