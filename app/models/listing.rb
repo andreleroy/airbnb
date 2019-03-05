aadccdeb402dade6d9876e43cc6936bea8acb7c2
Stephanie Meagher <stephanie.meagher@gmail.com>
2019-03-05 11:30:29 +0100

    add validations and relationships to Listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
+  has_many :photos, :bookings
+  validates :name, :description, :capacity, :addresss, :nightly_rate, :latitude, :longitude, presence: true
+  validates :type, presence: true, inclusion: { in: ["Apartment", "House", "Secondary unit", "Unique space", "Bed and breakfast", "Boutique hotel"] }
+  validates :capacity, :nightly_rate, numericality: { only_integer: true }
 end
