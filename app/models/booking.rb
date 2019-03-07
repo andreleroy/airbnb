00edfad803e737d15a071bb78dad291327682e13
Jennifer Lynch <jl.lynch9@gmail.com>
2019-03-07 20:03:18 +0100

    added css and links ... tbc!

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :listing
   belongs_to :user
+
+  # def duration(id)
+  #   @booking = Booking.find(id)
+  #   days = @booking.checkout_date - @booking.checkin_date
+  #   days.to_i
+  # end
 end
