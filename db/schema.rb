89ad7e7b95ef400d6f652b54e1a419d500f561c7
Jennifer Lynch <jl.lynch9@gmail.com>
2019-03-07 11:24:46 +0100

    started the form

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_03_06_130753) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +40,6 @@ ActiveRecord::Schema.define(version: 2019_03_06_130753) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.json "photos"
     t.float "latitude"
     t.float "longitude"
     t.index ["user_id"], name: "index_listings_on_user_id"
