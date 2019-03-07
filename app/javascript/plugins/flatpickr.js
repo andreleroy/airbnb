881bea86b6f4cf265016f8618cedbe4377a0ff2f
Maxime Friess <maxime@emeraldstay.com>
2019-03-07 16:43:49 +0100

    datepicker working

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,8 +1,4 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/themes/airbnb.css"; // Note this is important!
 
-flatpickr(".datepicker", {
-  altInput: true;
-
-
-});
+flatpickr(".datepicker", {});
